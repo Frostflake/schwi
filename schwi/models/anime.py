80ca134a720d0ce3eba75f9c7d0b9c877e85f19d
Frostflake <57190472+Frostflake@users.noreply.github.com>
2020-02-20 14:37:52 -0500

    Added additional info and comments to anime model

--- schwi/models/anime.py
+++ schwi/models/anime.py
@@ -1,19 +1,23 @@
 import json
 
 class Anime():
-	def __init__(name):
-		self.id = 0
-		self.name = name
-		self.year = None
-		self.status = "Plan to Watch"
-		self.score = None
-		self.progress = None
-		self.episodecount = None
-		self.startdate = None
-		self.enddate = None
-		self.rewatchcont = 0
-		self.notes = ""
-		self.externals = {}
-		
-	def Json():
-		return ""
+    def __init__(self, name):
+        self.id = 0 # The internal ID
+        self.name = name # The base Japanese name
+        self.year = None # The inital release year
+        self.status = "Plan to Watch" # The status of this entry
+        self.score = None # Our score for this entry
+        self.progress = None # Our progress for this entry
+        self.episode_count = None # The total number of episodes
+        self.start_date = None # When did we start watching?
+        self.end_date = None # When did we stop watching?
+        self.rewatch_count = 0 # How many times have we rewatched this?
+        self.notes = "" # Custom notes
+        self.externals = {} # Information about external services
+        self.alternate_names = [] # Do we have other names for this?
+        self.custom_categories = [] # Our custom categories
+        self.genres = [] # This entry's genres
+        self.tags = [] # This entry's tags
+
+    def Json():
+        return ""
