9b1dea4b1590c9ae420fca2aee37c34f3d2ac433
Frostflake <57190472+Frostflake@users.noreply.github.com>
2020-02-20 15:02:43 -0500

    Semi-functional anime class model

--- schwi/models/anime.py
+++ schwi/models/anime.py
@@ -1,9 +1,17 @@
-#import json
-
+import json
+from os import path
 
 class Anime():
     def __init__(self, name):
-        self.id = 0  # The internal ID
+        # Determine the next ID to use
+        # TODO: Refactor this into a general function
+        global_config = {}
+        with open(path.join(path.expanduser("~"), ".config", "schwi") + '/db/global.json') as json_file:
+            global_config = json.load(json_file)
+            self.id = global_config["Next Anime ID"]  # The internal ID
+            global_config["Next Anime ID"] += 1
+        with open(path.join(path.expanduser("~"), ".config", "schwi") + '/db/global.json', 'w') as json_file:
+            json.dump(global_config, json_file, indent=4)
         self.name = name  # The base Japanese name
         self.year = None  # The inital release year
         self.status = "Plan to Watch"  # The status of this entry
@@ -19,6 +27,24 @@ class Anime():
         self.custom_categories = []  # Our custom categories
         self.genres = []  # This entry's genres
         self.tags = []  # This entry's tags
+        self.Save()
 
-    def Json():
-        return ""
+    def Save(self):
+        self_dict = {}
+        self_dict["Name"] = self.name
+        self_dict["Release Year"] = self.year
+        self_dict["Status"] = self.status
+        self_dict["Score"] = self.score
+        self_dict["Progress"] = self.progress
+        self_dict["Episode Count"] = self.episode_count
+        self_dict["Start Date"] = self.start_date
+        self_dict["End Date"] = self.end_date
+        self_dict["Rewatch Count"] = self.rewatch_count
+        self_dict["Notes"] = self.notes
+        self_dict["External Services"] = self.externals
+        self_dict["Alternate Names"] = self.alternate_names
+        self_dict["Custom"] = self.custom_categories
+        self_dict["Genres"] = self.genres
+        self_dict["Tags"] = self.tags
+        with open(path.join(path.expanduser("~"), ".config", "schwi") + '/db/anime/' + str(self.id) + '.json', 'w') as json_file:
+            json.dump(self_dict, json_file, indent=4)
